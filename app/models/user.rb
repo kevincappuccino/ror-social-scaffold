75e712944190dfb18884a30b917bf6bfcf12737f
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-09-03 10:13:22 -0300

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,27 +12,27 @@ class User < ApplicationRecord
   has_many :invitations
   has_many :received_invitations, class_name: :Invitation, foreign_key: :friend_id
 
-def friends
-  friends_arr = invitations.map { |request| request.friend_id if request.accepted == true }
-  friends_arr += received_invitations.map { |request| request.user_id if request.accepted == true }
-  friends_arr.compact
-end
+  def friends
+    friends_arr = invitations.map { |request| request.friend_id if request.accepted == true }
+    friends_arr += received_invitations.map { |request| request.user_id if request.accepted == true }
+    friends_arr.compact
+  end
 
-def pending_requests
-  invitations.map { |request| request.friend unless request.accepted }.compact
-end
+  def pending_requests
+    invitations.map { |request| request.friend unless request.accepted }.compact
+  end
 
-def friend_requests
-  received_invitations.map { |request| request.user unless request.accepted }.compact
-end
+  def friend_requests
+    received_invitations.map { |request| request.user unless request.accepted }.compact
+  end
 
-def confirm_friend(user)
-  confrimation = received_invitations.find { |request| request.user == user }
-  confrimation.accepted = true
-  confrimation.save
-end
+  def confirm_friend(user)
+    confrimation = received_invitations.find { |request| request.user == user }
+    confrimation.accepted = true
+    confrimation.save
+  end
 
-def is_friend?(user)
-  friends.include?(user.id)
-end
+  def is_friend?(user)
+    friends.include?(user.id)
+  end
 end
